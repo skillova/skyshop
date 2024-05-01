ab3339163c948ddf9ec5439ce90a6963598acc6e
Skillova <skillova@yandex.ru>
2024-05-01 11:22:05 +0000

    добалвен контроллер, который отвечает за отображение контактной информации.

--- catalog/views.py
+++ catalog/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'You have new message from {name}({email}): {message}')
+    return render(request, 'catalog/contacts.html')
