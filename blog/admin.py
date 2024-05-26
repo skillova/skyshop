ed9fa47ffdf406d2d10575aafadc8a3fb146e90a
Skillova <skillova@yandex.ru>
2024-05-26 09:06:10 +0000

    register model blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "slug", "preview", "created_at", "sign_publication", "is_published")
