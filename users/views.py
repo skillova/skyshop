5a252092950b74ce655582f53e8bdfacf762f05b
Skillova <skillova@yandex.ru>
2024-06-12 14:41:18 +0000

    create controller registering user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy("users:login")
+
+    # def form_valid(self, form):
+    #     if form.is_valid():
+    #         new_blog = form.save()
+    #         new_blog.slug = slugify(new_blog.title)
+    #         new_blog.save()
+    #     return super().form_valid(form)
